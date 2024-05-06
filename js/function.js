eba9b49660d68e51d173162834f8b468a4418f16
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-06 17:09:33 +0200

    m-bonus-2 added each member has his own card with characteristics

--- js/function.js
+++ js/function.js
@@ -1,8 +1,8 @@
 function printPage (name, job, img) {
     div.innerHTML += 
-    `<div class="card">
+    `<div class="card text-center p-2">
         <h1>${name}</h1> 
         <h2>${job}</h2>
-        <img>${img}</img>
+        <img src="../img/${img}">${img}</img>
     </div>`
 }
