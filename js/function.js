1d979f11cf31a671c5ed074f6a6dd432f3e9dc07
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-06 14:50:24 +0200

    m-0 added array with inside 6 objects with string nome ruolo e img

--- js/function.js
+++ js/function.js
@@ -1,1 +1,35 @@
 console.log("Ciao Giuseppe")
+
+let arrayObjects = [
+    {
+        Nome: "Wayne Barnett",
+        Ruolo: "Founder & CEO",
+        Immagine: ""
+    },
+    {
+        Nome: "Angela Caroll",
+        Ruolo: "Chief Editor",
+        Immagine: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        Nome: "Walter Gordon",
+        Ruolo: "Office Manager",
+        Immagine: "walter-gordon-office-manager.jpg"
+    },
+    {
+        Nome: "Angela Lopez",
+        Ruolo: "Social Media Manager",
+        Immagine: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        Nome: "Scott Estrada",
+        Ruolo: "Developer",
+        Immagine: "scott-estrada-developer.jpg"
+    },
+    {
+        Nome: "Barbara Ramos",
+        Ruolo: "Graphic Designer",
+        Immagine: "barbara-ramos-graphic-designer.jpg"
+    },
+]
+console.log(arrayObjects)
