4524c806bca1f2c9a9634423369cd90e68888097
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-06 16:17:54 +0200

    m-2

--- js/function.js
+++ js/function.js
@@ -1,47 +1,5 @@
-console.log("Ciao Giuseppe")
-
-//creo lista che contiene 6 oggetti con dentro ognuno delle caratteristiche diverse
-let arrayObjects = [
-    {
-        Nome: "Wayne Barnett",
-        Ruolo: "Founder & CEO",
-        Immagine: "wayne-barnett-founder-ceo.jpg"
-    },
-    {
-        Nome: "Angela Caroll",
-        Ruolo: "Chief Editor",
-        Immagine: "angela-caroll-chief-editor.jpg"
-    },
-    {
-        Nome: "Walter Gordon",
-        Ruolo: "Office Manager",
-        Immagine: "walter-gordon-office-manager.jpg"
-    },
-    {
-        Nome: "Angela Lopez",
-        Ruolo: "Social Media Manager",
-        Immagine: "angela-lopez-social-media-manager.jpg"
-    },
-    {
-        Nome: "Scott Estrada",
-        Ruolo: "Developer",
-        Immagine: "scott-estrada-developer.jpg"
-    },
-    {
-        Nome: "Barbara Ramos",
-        Ruolo: "Graphic Designer",
-        Immagine: "barbara-ramos-graphic-designer.jpg"
-    },
-]
-//faccio console.log della lista degli oggetti finita
-console.log(arrayObjects)
-
-//creo ciclo con contatore per scorrere in tutta la lista arrayObjects
-for (let i = 0; i < arrayObjects.length; i++) {
-    //creo variabile di appoggio per gli arrayObjects in posizione i
-    let arrayTeam = arrayObjects[i]
-    //creo ciclo che mi identifica la chiave e la proprietà nella lista in tutti gli indici i
-    for(let key in arrayTeam) {
-        console.log("La chiave è:", key, "La proprietà è: ", arrayTeam[key] )
-    }
+function printPage (name, job, img) {
+    h1.innerText += name
+    h2.innerText += job
+    h3.innerText += img
 }
---
+++ js/main.js
@@ -0,0 +1,52 @@
+console.log("Ciao Giuseppe")
+
+let h1 = document.querySelector("h1")
+let h2 = document.querySelector("h2")
+let h3 = document.querySelector("h3")
+
+//creo lista che contiene 6 oggetti con dentro ognuno delle caratteristiche diverse
+let arrayObjects = [
+    {
+        Nome: "Wayne Barnett",
+        Ruolo: "Founder & CEO",
+        Immagine: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        Nome: "Angela Caroll",
+        Ruolo: "Chief Editor",
+        Immagine: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        Nome: "Walter Gordon",
+        Ruolo: "Office Manager",
+        Immagine: "walter-gordon-office-manager.jpg"
+    },
+    {
+        Nome: "Angela Lopez",
+        Ruolo: "Social Media Manager",
+        Immagine: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        Nome: "Scott Estrada",
+        Ruolo: "Developer",
+        Immagine: "scott-estrada-developer.jpg"
+    },
+    {
+        Nome: "Barbara Ramos",
+        Ruolo: "Graphic Designer",
+        Immagine: "barbara-ramos-graphic-designer.jpg"
+    },
+]
+//faccio console.log della lista degli oggetti finita
+console.log(arrayObjects)
+
+//creo ciclo con contatore per scorrere in tutta la lista arrayObjects
+for (let i = 0; i < arrayObjects.length; i++) {
+    //creo variabile di appoggio per gli arrayObjects in posizione i
+    let arrayTeam = arrayObjects[i]
+    //creo ciclo che mi identifica la chiave e la proprietà nella lista in tutti gli indici i
+    for(let key in arrayTeam) {
+        console.log("La chiave è:", key, "La proprietà è: ", arrayTeam[key])
+    }
+    printPage(arrayTeam.Nome, arrayTeam.Ruolo, arrayTeam.Immagine)
+}
