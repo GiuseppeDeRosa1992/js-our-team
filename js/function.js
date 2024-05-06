e2c77bc110362a13a9410475f1b0459b0e52dfe6
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-05-06 15:23:50 +0200

    m-1 added cycle for with i, created variable arrayTeam that contain arrayObjects in position i,  and added cycle for with variable key

--- js/function.js
+++ js/function.js
@@ -1,10 +1,11 @@
 console.log("Ciao Giuseppe")
 
+//creo lista che contiene 6 oggetti con dentro ognuno delle caratteristiche diverse
 let arrayObjects = [
     {
         Nome: "Wayne Barnett",
         Ruolo: "Founder & CEO",
-        Immagine: ""
+        Immagine: "wayne-barnett-founder-ceo.jpg"
     },
     {
         Nome: "Angela Caroll",
@@ -32,4 +33,15 @@ let arrayObjects = [
         Immagine: "barbara-ramos-graphic-designer.jpg"
     },
 ]
+//faccio console.log della lista degli oggetti finita
 console.log(arrayObjects)
+
+//creo ciclo con contatore per scorrere in tutta la lista arrayObjects
+for (let i = 0; i < arrayObjects.length; i++) {
+    //creo variabile di appoggio per gli arrayObjects in posizione i
+    let arrayTeam = arrayObjects[i]
+    //creo ciclo che mi identifica la chiave e la proprietà nella lista in tutti gli indici i
+    for(let key in arrayTeam) {
+        console.log("La chiave è:", key, "La proprietà è: ", arrayTeam[key] )
+    }
+}
